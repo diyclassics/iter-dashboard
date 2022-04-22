871638dff82f6adfa062830216a929c7d7cced1f
Patrick J. Burns <patricks@diyclassics.org>
2022-04-21 21:00:53 -0400

    Update app

--- counts.py
+++ counts.py
@@ -4,6 +4,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from cltkreaders.lat import LatinTesseraeCorpusReader
 from latintools import preprocess
 
+# Define vocabulary
+vocabulary = ['pater', 'mater', 'filius', 'filia', 'auunculus', 'patruus']
+
 # Get corpus
 T = LatinTesseraeCorpusReader()
 
@@ -12,9 +15,12 @@ docs = [next(T.texts(file, preprocess=preprocess)) for file in T.fileids()]
 
 # Get metadata
 dates = [T.metadata('date', file) for file in T.fileids()]
-
-df = pd.DataFrame(dates, columns=['dates'], index=T.fileids())
+df = pd.DataFrame(dates, columns=['date'], index=T.fileids())
 df.to_csv('data/output/dates.tsv', sep='\t')
+
+modes = [T.metadata('mode', file) for file in T.fileids()]
+df = pd.DataFrame(modes, columns=['mode'], index=T.fileids())
+df.to_csv('data/output/modes.tsv', sep='\t')
 
 # Get approx counts
 # TODO: Get true counts
@@ -24,7 +30,6 @@ df = pd.DataFrame(totals, columns=['total'], index=T.fileids())
 df.to_csv('data/output/totals.tsv', sep='\t')
 
 # Vectorize
-vocabulary = ['pater', 'mater', 'filius', 'filia']
 vectorizer = CountVectorizer(vocabulary=vocabulary)
 X = vectorizer.fit_transform(docs)
 vocab = vectorizer.get_feature_names_out()
